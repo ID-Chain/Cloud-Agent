86435285dae7315482f236fbfb82f8f63be231cd
Bersant Deva <bersant.deva@tu-berlin.de>
2018-08-13 13:28:19 +0200

    Improved test-server. Firebase Token now passed as parameter.

--- firebase/test-server.js
+++ firebase/test-server.js
@@ -1,25 +1,27 @@
+require('dotenv').config();
 const admin = require('../persistence/fb-admin');
 
-var firebaseToken = 'eSve9rGdJD8:APA91bHYCSmpSFqBGRRpZrr2kM7ElVEYYw-YlKfy_vCptqaJ1AO2Znko_dqQI8n2J4sFoNs5aO0RoaOP4IhNkg-g0Jk6dg8ak__BDv3T5-_G0LB4D2RjRUJp9OXZTGT-0CvMnV6T9WtbShmPrSlHySHTDMebAN8clA';
+var token = process.argv[2];
+
 // See documentation on defining a message payload.
 var message = {
-    webpush: {
-        notification:{
-            title: "Portugal vs. Denmark",
-            body:  "5 to 1",
-            icon: "firebase-logo.png",
-        }
+    data: {
+        title: 'Portugal vs. Denmark',
+        body: '5 to 1',
+        icon: 'firebase-logo.png'
     },
-    token: firebaseToken
+    token: token
 };
 
-admin.messaging().send(message)
-    .then((response) => {
+admin
+    .messaging()
+    .send(message)
+    .then(response => {
         // Response is a message ID string.
         console.log('Successfully sent message:', response);
-        return response
+        return response;
     })
-    .catch((error) => {
+    .catch(error => {
         console.log('Error sending message:', error);
-        return error
+        return error;
     });
