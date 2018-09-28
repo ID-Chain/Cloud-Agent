6b678f7bb72fe7d8a4073a13fa288715f788e03e
Bersant Deva <bersant.deva@tu-berlin.de>
2018-09-28 17:32:09 +0200

    added test framework for registering apps and sending messages via FCM

---
+++ test/0-test-vars.js
@@ -0,0 +1,14 @@
+/**
+ * IDChain Agent REST API
+ * Test Variables
+ */
+
+require('dotenv').config();
+const request = require('supertest');
+
+module.exports.serverURL = `http://${process.env.APP_HOST}:${process.env.APP_PORT}`;
+module.exports.agent = request.agent(module.exports.serverURL);
+
+module.exports.acceptHeader = { Accept: 'application/json' };
+module.exports.contentHeader = { 'Content-Type': 'application/json' };
+module.exports.bothHeaders = Object.assign({}, module.exports.acceptHeader, module.exports.contentHeader);
